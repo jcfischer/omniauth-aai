e92be023480041b9b313d28d08b839b61299f9b0
Claudio Beffa <claudio@beffa.ch>
2012-10-31 14:04:56 +0100

    test

--- spec/omniauth/strategies/brute_force_spec.rb
+++ spec/omniauth/strategies/brute_force_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
-require 'capybara/rspec'
+#require 'capybara/rspec'
+
+describe "include gem" do
+  #needs sqllit in gems, but this conflicts with jruby
+  it "should not raise any exceptions" do
+   FileUtils.cp_r("spec/example_rails_app", "spec/test_app_with_active_record")
+   #`cd spec/test_app_with_active_record; bundle install --gemfile=Gemfile`
+   `cd spec/test_app_with_active_record; bundle exec  --gemfile=Gemfile rails generate aai:install`
+   `cd spec/test_app_with_active_record; bundle exec  --gemfile=Gemfile rake db:migrate` 
+   FileUtils.rm_r("spec/test_app_with_active_record")
+  end
+
+  it "should not raise any exceptions if not persist gem with app" do
+    FileUtils.cp_r("spec/example_rails_app", "spec/test_app_no_persist")
+    `cd spec/test_app_no_persist; bundle exec rails generate aai:install --persist false`
+    FileUtils.rm_r("spec/test_app_no_persist")
+  end
+end
 
 #describe "test app ar" do
 #  before(:all) do
