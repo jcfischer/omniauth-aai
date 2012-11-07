1913dbe8a70fc6f0601b06ed55107eef32c427a4
Christian Rohrer <christian.rohrer@switch.ch>
2012-11-07 15:27:59 +0100

    fixed ship typo in user template

--- lib/generators/aai/templates/user.rb
+++ lib/generators/aai/templates/user.rb
@@ -55,7 +55,7 @@ class User <%= options[:persist]  ? "< ActiveRecord::Base" : "" %>
 
  <% end %>
 
-  #def ship_session_id
+  #def shib_session_id
   #  aai["extra"]["raw_info"]['Shib-Session-ID']
   #end
   #
