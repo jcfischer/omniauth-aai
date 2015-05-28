9a5b1e1af0246ea0982df71bbe74f1e439c01b89
Christian Rohrer <christian.rohrer@switch.ch>
2015-05-28 10:48:04 +0200

    Replace match with get|post in example routes

--- spec/example_app/config/routes.rb
+++ spec/example_app/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  match '/auth/logout',  :to => 'session#destroy', :as => 'logout'
-  match '/auth/failure', :to => 'session#failure', :as => 'auth_failure'
-  match '/auth/:provider/callback', :to => 'session#create', :as => 'auth_callback'
+  get '/auth/logout',  :to => 'session#destroy', :as => 'logout'
+  get '/auth/failure', :to => 'session#failure', :as => 'auth_failure'
+  post '/auth/:provider/callback', :to => 'session#create', :as => 'auth_callback'
   get "welcome/index"
   get "welcome/protected"
 
