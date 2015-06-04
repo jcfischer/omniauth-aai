8c44fc2da370e8222f1a0c9b04f7a8e3e43778fb
Christian Rohrer <christian.rohrer@switch.ch>
2015-06-04 09:27:33 +0200

    Move has_current_user to omniauth-aai scope

---
+++ lib/omniauth-aai/has_current_user.rb
@@ -0,0 +1,47 @@
+module OmniauthAai
+  module HasCurrentUser
+    def has_current_user
+      helper_method :current_user
+      include InstanceMethods
+    end
+    module InstanceMethods
+      @user = nil
+
+      def current_user
+        return nil unless @user.present? || session[:current_user].present?
+        @user = @user || User.unmarshal( session[:current_user] )
+        @user
+      end
+
+      # Set the current user
+      def current_user=(user)
+        @user = user
+        session[:current_user] = @user.marshal unless @user.nil?
+      end
+
+      # Authenticate User
+      def authenticate!
+        return if authenticated?
+        session[:return_to] = request.url
+        if Rails.env.development?
+          redirect_to "/auth/developer"
+        else
+          redirect_to "/auth/aai"
+        end
+      end
+
+      # User authenticated?
+      def authenticated?
+        return true if self.current_user.present? && self.current_user.uid.present?
+        return false
+      end
+    end
+  end
+
+  begin
+    ActionController::Base.extend HasCurrentUser
+  rescue NameError => e
+    puts "ActionController undefined"
+  end
+
+end
