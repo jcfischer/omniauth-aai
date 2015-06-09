ded4322be96899baadfa6cf101f7a4673a4e61c2
Christian Rohrer <christian.rohrer@switch.ch>
2015-06-09 10:06:42 +0200

    Re-enable has_current_user method to include in ActionControllers

--- spec/example_rails4_app/app/controllers/application_controller.rb
+++ spec/example_rails4_app/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  include Omniauth::HasCurrentUser
+  # include Omniauth::HasCurrentUser
+  has_current_user
 end
