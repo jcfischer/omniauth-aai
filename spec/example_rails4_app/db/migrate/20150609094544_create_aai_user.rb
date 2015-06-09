dfb8f5c307f10583236307183f7556ef372bf8f5
Christian Rohrer <christian.rohrer@switch.ch>
2015-06-09 11:51:36 +0200

    Update Rails4 example app

---
+++ spec/example_rails4_app/db/migrate/20150609094544_create_aai_user.rb
@@ -0,0 +1,26 @@
+class CreateAaiUser < ActiveRecord::Migration
+  def change
+
+    create_table(:users) do |t|
+      t.string :uid
+      t.string :unique_id
+      t.string :persistent_id
+      t.string :email
+      t.string :first_name
+      t.string :last_name
+      t.string :home_organization
+      t.text :raw_data
+      t.timestamps
+    end
+
+    # if ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
+    #   add_column :users, :raw_data, :json
+    # else
+    #   add_column :users, :raw_data, :string
+    # end
+
+    add_index :users, :uid, unique: true
+    add_index :users, :unique_id, unique: true
+    add_index :users, :persistent_id, unique: true
+  end
+end
